8dc694c2d25a5739b09874c34f4da7173596fcb0
Mingyue Chen <mychen@thoughtworks.com>
2018-09-03 20:40:15 +0800

    [Mingyue]Modify the tests in FloatingTypeTest to make them pass.

--- src/test/java/com/cultivation/javaBasic/FloatingTypeTest.java
+++ src/test/java/com/cultivation/javaBasic/FloatingTypeTest.java
@@ -15,7 +15,7 @@ class FloatingTypeTest {
 
         // TODO: Please change the result to pass the test.
         // <!--start
-        final int expected = Integer.MAX_VALUE;
+        final int expected = 2;
         // --end-->
 
         assertEquals(expected, integer);
@@ -41,7 +41,7 @@ class FloatingTypeTest {
 
         // TODO: Please change the result to pass the test.
         // <!--start
-        final int expected = Integer.MAX_VALUE;
+        final int expected = 2;
         // --end-->
 
         assertEquals(expected, integer);
@@ -54,7 +54,7 @@ class FloatingTypeTest {
 
         // TODO: Please call some method to round the floating point number.
         // <!--start
-        final long rounded = Long.MAX_VALUE;
+        final long rounded = Math.round(floatingPointNumber);
         // --end-->
 
         assertEquals(3L, rounded);
@@ -63,13 +63,17 @@ class FloatingTypeTest {
     @SuppressWarnings("unused")
     private boolean isNan(double realNumber) {
         // TODO: please implement the method to pass the test.
-        throw new NotImplementedException();
+        return Double.isNaN(realNumber);
     }
 
     @SuppressWarnings("unused")
     private boolean isInfinity(double realNumber) {
         // TODO: please implement the method to pass the test.
-        throw new NotImplementedException();
+//        if(realNumber == Double.POSITIVE_INFINITY || realNumber == Double.NEGATIVE_INFINITY){
+//            return true;
+//        }
+//        return false;
+        return Double.isInfinite(realNumber);
     }
 
     /*
